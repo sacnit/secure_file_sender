078b685e40833e4279c6b5cbaccfda1169559e47
sacnit <casperwright108@gmail.com>
2025-01-17 23:57:07 +0000

    added prefix checking for arguments

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,30 @@
 // use std::net::TcpStream;
 use std::env;
 
-fn parse_arguments(arguments: Vec<String>) -> bool {
-    let mut formatted_arguments: Vec<String> = Vec::new();
-    for argument in 1..arguments.len() {
-        formatted_arguments.push(arguments[argument].clone());
+struct Parameters {
+    r#type: Option<bool>,
+    ultrapeer: Option<String>,
+}
+
+// Implement length checks and error handling for missing values
+fn parse_arguments(arguments: Vec<String>, parameters: &mut Parameters) {
+    for argument_index in 1..arguments.len() {
+        let current_argument = arguments[argument_index].clone();
+        let current_argument_prefix = &current_argument[0..2];
+        if current_argument_prefix == "--" {
+            if &current_argument[2..] == "type" {
+                parameters.r#type = if arguments[argument_index + 1] == "ultrapeer".to_string() {Some(true)} else {Some(false)};
+            }
+        }
     }
-    dbg!(formatted_arguments);
-    return true
 }
 
 fn main() {
-    let arguments: Vec<String> = env::args().collect();
-    parse_arguments(arguments);
+    let mut parameters = Parameters {
+        r#type: None,
+        ultrapeer: None,
+    };
+    let arguments: Vec<String> = env::args().collect(); 
+    parse_arguments(arguments, &mut parameters);
     println!("Hello, world!");
 }
