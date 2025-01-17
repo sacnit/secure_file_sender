9ad543ff878c13e0e2300bda962e57e6df60cd51
sacnit <casperwright108@gmail.com>
2025-01-17 18:49:23 +0000

    Made dockerfile and compose file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+// use native_tls::TlsConnector;
+// use std::io::{Read, Write};
+// use std::net::TcpStream;
+use std::env;
+
+fn parse_arguments(arguments: Vec<String>) -> bool {
+    let mut formatted_arguments: Vec<String> = Vec::new();
+    for argument in 1..arguments.len() {
+        formatted_arguments.push(arguments[argument].clone());
+    }
+    dbg!(formatted_arguments);
+    return true
+}
+
 fn main() {
+    let arguments: Vec<String> = env::args().collect();
+    parse_arguments(arguments);
     println!("Hello, world!");
 }
